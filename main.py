68824c0adaca187195e6e629cf794cc6c8b0c664
Deireschlet <164866655+Deireschlet@users.noreply.github.com>
2025-05-30 19:22:05 +0200

    [UPDATE] minor project updates

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import pandas as pd
 import datetime as dt
 from setup import logger, config
 from setup.logger import log_call
-from src.data_handler import fetch_stock_data, plot_1d_distribution, plot_1d_data
-from src.computation import black_scholes, annualized_historical_vola, simulate_stock_price, discounted_avg_payoff \
-    , payoff
+from src.data_handler import get_user_input, fetch_stock_data
+from src.monte_carlo_pricing import mc_pricing
+from src.least_square_mc import lsm_american
 
 
 @log_call(logger)
 def main():
-    pass
+    option_type, ticker, strike_price, maturity, risk_free_rate, vola = get_user_input()
+    df = fetch_stock_data(ticker, "2023-01-01", dt.date.today().strftime("%Y-%m-%d"))
+
+    # todo: create data frame to add all return values together
+
+    mc_pricing(option_type, df, strike_price, maturity, risk_free_rate, vola)
+    lsm_american(option_type, df, strike_price, maturity, risk_free_rate, vola)
 
 if __name__ == "__main__":
     main()
